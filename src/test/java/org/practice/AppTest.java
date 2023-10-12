2cc254a575838c85e78b6eb0b2ea7fadde18c1a9
Sonu8726 <sonukumar4100.sk@gmail.com>
2023-10-12 11:14:51 +0530

    added the Selenium basic TCs

--- src/test/java/org/practice/AppTest.java
+++ src/test/java/org/practice/AppTest.java
@@ -1,17 +1,47 @@
 package org.practice;
 
-public class AppTest{
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-    int count;
+import java.time.Duration;
+
+public class AppTest {
+
+    static WebDriver driver;
     public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
 
-        //Create a class and instantiate an object.
-        AppTest aptst = new AppTest();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        aptst.count=10;
+        AppTest appTest = new AppTest();
+        appTest.launchAWebPage(driver, "https://the-internet.herokuapp.com/");
 
-        System.out.println(aptst.count);
+        appTest.addRemoveTheElement(driver);
 
+        appTest.closeTheLaunchedBrowser(driver);
     }
+
+    private void addRemoveTheElement(WebDriver driver) {
+        driver.findElement(By.linkText("Add/Remove Elements")).click();
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+        driver.findElement(By.id("elements")).isDisplayed();
+        driver.findElement(By.xpath("//button[text()='Add Element']")).click();
+    }
+
+    private void launchAWebPage(WebDriver driver, String url){
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+
+
+    private void closeTheLaunchedBrowser(WebDriver driver){
+        driver.quit();
+    }
+
 
 }
